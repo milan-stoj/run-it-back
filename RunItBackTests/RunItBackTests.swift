6b71aaf6f2e1c8df249ed084f568f4e2f7516efd
Milan Stojanovic <milanstoj94@gmail.com>
2025-12-11 14:17:51 -0600

    refactor: rename project

---
+++ RunItBackTests/RunItBackTests.swift
@@ -0,0 +1,17 @@
+//
+//  run_it_backTests.swift
+//  run-it-backTests
+//
+//  Created by Milan Stojanovic on 12/11/25.
+//
+
+import Testing
+@testable import run_it_back
+
+struct run_it_backTests {
+
+    @Test func balancesTeams() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
